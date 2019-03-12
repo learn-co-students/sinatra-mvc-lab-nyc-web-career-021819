a876d044ffabf92bb61375fe7da635279e781492
Kate Kim <katekim5809@gmail.com>
2019-03-12 16:02:27 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,28 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+ # POST '/piglatinize'
+ #   returns a 200 status code (FAILED - 7)
+ #   displays the pig latinized phrase upon form submission (FAILED - 8)
+ # POST '/piglatinize' again
+ #   returns a 200 status code (FAILED - 9)
+ #   displays the pig latinized phrase upon form submission (FAILED - 10)
+ #
+
+
+  get '/' do
+
+    erb :user_input
+  end
+
+  post "/piglatinize" do
+    text_from_user = params[:user_phrase]
+    @analyzed_text = text_from_user
+    erb :piglatinize
+
+  end
+
+
 end
